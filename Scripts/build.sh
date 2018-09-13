3606936a794085e6d1de21d2842ddd842a03ca20
Remco Kok <kok0047@hz.nl>
2018-09-13 11:10:59 +0200

    test-2

--- Scripts/build.sh
+++ Scripts/build.sh
@@ -1,19 +1,37 @@
 #! /bin/sh
 
-# Example build script for Unity3D project. See the entire example: https://github.com/JonathanPorta/ci-build
+PROJECT_PATH=$(pwd)/$UNITY_PROJECT_PATH
+UNITY_BUILD_DIR=$(pwd)/Build
+LOG_FILE=$UNITY_BUILD_DIR/unity-win.log
 
-# Change this the name of your project. This will be the name of the final executables as well.
-project="ci-build"
 
-echo "Attempting to build $project for Windows"
+ERROR_CODE=0
+echo "Items in project path ($project_path):"
+ls "$project_path"
+
+
+echo "Building project for Windows..."
+mkdir $UNITY_BUILD_DIR
 /Applications/Unity/Unity.app/Contents/MacOS/Unity \
   -batchmode \
   -nographics \
   -silent-crashes \
-  -logFile $(pwd)/unity.log \
-  -projectPath $(pwd) \
-  -buildWindowsPlayer "$(pwd)/Build/windows/$project.exe" \
-  -quit
+  -logFile \
+  -projectPath "$PROJECT_PATH" \
+  -buildWindows64Player  "$(pwd)/build/win/ci-build.exe" \
+  -quit \
+  | tee "$LOG_FILE"
+  
+if [ $? = 0 ] ; then
+  echo "Building Windows exe completed successfully."
+  ERROR_CODE=0
+else
+  echo "Building Windows exe failed. Exited with $?."
+  ERROR_CODE=1
+fi
 
-echo 'Logs from build'
-cat $(pwd)/unity.log
+#echo 'Build logs:'
+#cat $LOG_FILE
+
+echo "Finishing with code $ERROR_CODE"
+exit $ERROR_CODE
--- Scripts/install.sh
+++ Scripts/install.sh
@@ -1,12 +1,41 @@
-
 #! /bin/sh
 
-# Example install script for Unity3D project. See the entire example: https://github.com/JonathanPorta/ci-build
+# See $BASE_URL/$HASH/unity-$VERSION-$PLATFORM.ini for complete list
+# of available packages, where PLATFORM is `osx` or `win`
+BASE_URL=https://beta.unity3d.com/download/46dda1414e51
+VERSION=2017.2.0f3
 
-# This link changes from time to time. I haven't found a reliable hosted installer package for doing regular
-# installs like this. You will probably need to grab a current link from: http://unity3d.com/get-unity/download/archive
-echo 'Downloading from http://netstorage.unity3d.com/unity/3757309da7e7/MacEditorInstaller/Unity-5.2.2f1.pkg: '
-curl -o Unity.pkg http://netstorage.unity3d.com/unity/3757309da7e7/MacEditorInstaller/Unity-5.2.2f1.pkg
+UNITY_OSX_PACKAGE="MacEditorInstaller/Unity-$VERSION.pkg"
+UNITY_WINDOWS_TARGET_PACKAGE="MacEditorTargetInstaller/UnitySetup-Windows-Support-for-Editor-$VERSION.pkg"
 
-echo 'Installing Unity.pkg'
-sudo installer -dumplog -package Unity.pkg -target /
+
+download() {
+	
+	FILE=$1
+	URL="$BASE_URL/$FILE"
+
+	#download package if it does not already exist in cache
+	if [ ! -e $UNITY_DOWNLOAD_CACHE/`basename "$FILE"` ] ; then
+		echo "$FILE does not exist. Downloading from $URL: "
+		curl -o $UNITY_DOWNLOAD_CACHE/`basename "$FILE"` "$URL"
+	else
+		echo "$FILE Exists. Skipping download."
+	fi
+}
+
+install() {
+	PACKAGE=$1
+	download "$PACKAGE"
+	
+	echo "Installing "`basename "$PACKAGE"`
+	sudo installer -dumplog -package $UNITY_DOWNLOAD_CACHE/`basename "$PACKAGE"` -target /
+}
+
+
+
+echo "Contents of Unity Download Cache:"
+ls "$UNITY_DOWNLOAD_CACHE"
+
+echo "Installing Unity..."
+install "$UNITY_OSX_PACKAGE"
+install "$UNITY_WINDOWS_TARGET_PACKAGE"
